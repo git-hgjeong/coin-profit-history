27c772122ec971881b47c45e075f90e3ccdf85a7
git-hgjeong <code.hgjeong@gmail.com>
2024-03-25 22:55:06 +0900

    파일로생성

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import pandas as pd
+from datetime import datetime
+
+now = datetime.now()
+
+export_file_name = now.strftime('%Y-%m-%d_%H')
+print(export_file_name)
 
 # 엑셀 파일 경로 지정
 file_path = 'upbit.xlsx'
@@ -13,6 +19,7 @@ df[11] = df[8].astype(str).str.replace('KRW', '').str.replace(',', '').astype(in
 # 매수인 경우 8번째 컬럼 값을 음수로 처리
 df[12] = df.apply(lambda x: -x[11] if x[3] == '매수' else x[11], axis=1)
 print(df)
+df.to_excel(excel_writer=f'{export_file_name}.xlsx')
 
 # 첫 번째 컬럼의 값으로 그룹을 만들고, 조정된 합계의 합계를 계산
 grouped_sum_day = df.groupby(10)[12].sum()
